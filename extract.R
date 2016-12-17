54b77345ae54a2aa0264bc6bd472ede2bc48ba6e
Xiaoting Kuang <xiaotingkuangcu@gmail.com>
2016-12-17 17:43:19 -0500

    Add files via upload

---
+++ extract.R
@@ -0,0 +1,126 @@
+normal <- read.csv("2016-12-15_12-00-36.csv", header = TRUE, stringsAsFactors = FALSE)
+export <- read.csv("2016-12-15_12-00-32.csv", header = TRUE, stringsAsFactors = FALSE)
+par(mfrow=c(2,2))
+plot(normal$loggingSample, normal$motionYaw)
+lines(normal$loggingSample, normal$motionYaw)
+plot(export$loggingSample, export$motionYaw)
+lines(export$loggingSample, export$motionYaw)
+summary(lm(normal$motionYaw~normal$loggingSample))
+summary(lm(export$motionYaw~export$loggingSample))
+# a weird W shape distribution, what is this?????
+
+plot(normal$loggingSample, normal$motionRoll)
+lines(normal$loggingSample, normal$motionRoll)
+plot(export$loggingSample, export$motionRoll)
+lines(export$loggingSample, export$motionRoll)
+summary(lm(normal$loggingSample~normal$motionRoll))
+summary(lm(export$loggingSample~export$motionRoll))
+# looks like an M, dense on the top
+
+plot(normal$loggingSample, normal$motionPitch)
+lines(normal$loggingSample, normal$motionPitch)
+plot(export$loggingSample, export$motionPitch)
+lines(export$loggingSample, export$motionPitch)
+summary(lm(normal$loggingSample~normal$motionPitch))
+summary(lm(export$loggingSample~export$motionPitch))
+# like an upside down version of the previous one, dense on the bottom
+
+plot(normal$loggingSample, normal$motionRotationRateX)
+lines(normal$loggingSample, normal$motionRotationRateX)
+plot(export$loggingSample, export$motionRotationRateX)
+lines(export$loggingSample, export$motionRotationRateX)
+summary(lm(normal$loggingSample~normal$motionRotationRateX))
+summary(lm(export$loggingSample~export$motionRotationRateX))
+
+plot(normal$loggingSample, normal$motionRotationRateY)
+lines(normal$loggingSample, normal$motionRotationRateY)
+plot(export$loggingSample, export$motionRotationRateY)
+lines(export$loggingSample, export$motionRotationRateY)
+summary(lm(normal$loggingSample~normal$motionRotationRateY))
+summary(lm(export$loggingSample~export$motionRotationRateY))
+
+plot(normal$loggingSample, normal$motionRotationRateZ)
+lines(normal$loggingSample, normal$motionRotationRateZ)
+plot(export$loggingSample, export$motionRotationRateZ)
+lines(export$loggingSample, export$motionRotationRateZ)
+summary(lm(normal$loggingSample~normal$motionRotationRateZ))
+summary(lm(export$loggingSample~export$motionRotationRateZ))
+# dense around x = 0, the three look pretty similar
+
+plot(normal$loggingSample, normal$motionUserAccelerationX)
+lines(normal$loggingSample, normal$motionUserAccelerationX)
+plot(export$loggingSample, export$motionUserAccelerationX)
+lines(export$loggingSample, export$motionUserAccelerationX)
+summary(lm(normal$loggingSample~normal$motionUserAccelerationX))
+summary(lm(export$loggingSample~export$motionUserAccelerationX))
+
+plot(normal$loggingSample, normal$motionUserAccelerationY)
+lines(normal$loggingSample, normal$motionUserAccelerationY)
+plot(export$loggingSample, export$motionUserAccelerationY)
+lines(export$loggingSample, export$motionUserAccelerationY)
+summary(lm(normal$loggingSample~normal$motionUserAccelerationY))
+summary(lm(export$loggingSample~export$motionUserAccelerationY))
+
+plot(normal$loggingSample, normal$motionUserAccelerationZ)
+lines(normal$loggingSample, normal$motionUserAccelerationZ)
+plot(export$loggingSample, export$motionUserAccelerationZ)
+lines(export$loggingSample, export$motionUserAccelerationZ)
+summary(lm(normal$loggingSample~normal$motionUserAccelerationZ))
+summary(lm(export$loggingSample~export$motionUserAccelerationZ))
+# same behavior, like the rotation rates
+
+plot(normal$loggingSample, normal$motionQuaternionX)
+lines(normal$loggingSample, normal$motionQuaternionX)
+plot(export$loggingSample, export$motionQuaternionX)
+lines(export$loggingSample, export$motionQuaternionX)
+summary(lm(normal$loggingSample~normal$motionQuaternionX))
+summary(lm(export$loggingSample~export$motionQuaternionX))
+
+plot(normal$loggingSample, normal$motionQuaternionY)
+lines(normal$loggingSample, normal$motionQuaternionY)
+plot(export$loggingSample, export$motionQuaternionY)
+lines(export$loggingSample, export$motionQuaternionY)
+summary(lm(normal$loggingSample~normal$motionQuaternionY))
+summary(lm(export$loggingSample~export$motionQuaternionY))
+
+plot(normal$loggingSample, normal$motionQuaternionZ)
+lines(normal$loggingSample, normal$motionQuaternionZ)
+plot(export$loggingSample, export$motionQuaternionZ)
+lines(export$loggingSample, export$motionQuaternionZ)
+summary(lm(normal$loggingSample~normal$motionQuaternionZ))
+summary(lm(export$loggingSample~export$motionQuaternionZ))
+
+plot(normal$loggingSample, normal$motionQuaternionW)
+lines(normal$loggingSample, normal$motionQuaternionW)
+plot(export$loggingSample, export$motionQuaternionW)
+lines(export$loggingSample, export$motionQuaternionW)
+summary(lm(normal$loggingSample~normal$motionQuaternionW))
+summary(lm(export$loggingSample~export$motionQuaternionW))
+# the X one is all over the place, but bottom dense,
+# increased very fast, but dropped very fast in the end
+#a W shaped thing
+# same as Z
+
+
+plot(normal$loggingSample, normal$motionGravityX)
+lines(normal$loggingSample, normal$motionGravityX)
+plot(export$loggingSample, export$motionGravityX)
+lines(export$loggingSample, export$motionGravityX)
+summary(lm(normal$loggingSample, normal$motionGravityX))
+summary(lm(export$loggingSample, export$motionGravityX))
+
+plot(normal$loggingSample, normal$motionGravityY)
+lines(normal$loggingSample, normal$motionGravityY)
+plot(export$loggingSample, export$motionGravityY)
+lines(export$loggingSample, export$motionGravityY)
+summary(lm(normal$loggingSample~normal$motionGravityY))
+summary(lm(export$loggingSample~export$motionGravityY))
+
+plot(normal$loggingSample, normal$motionGravityZ)
+lines(normal$loggingSample, normal$motionGravityZ)
+plot(export$loggingSample, export$motionGravityZ)
+lines(export$loggingSample, export$motionGravityZ)
+summary(lm(normal$loggingSample~normal$motionGravityZ))
+summary(lm(export$loggingSample~export$motionGravityZ))
+# X and Y behave alike, but Z has an increasing trend
+
---
+++ final 1.R
@@ -0,0 +1,139 @@
+setwd("~/Nino Zhang/study at TC/fall 2016/EDM/final project")
+small <- read.csv("2016-12-15_12-00-32.csv", header = TRUE, stringsAsFactors = FALSE)
+small$ID <- "0"
+class(small)
+colnames(small)
+
+
+
+
+# logging sample is a sequence, 1:n
+
+plot(x <- small$loggingSample, y <- small$locationTimestamp_since1970)
+# location time stamp is increasing linearly
+
+plot(x <- small$loggingSample, y <- small$locationLatitude)
+hist(small$loggingSample, small$locationLatitude)
+# can't really see a pattern on location latitude, wut is dis? do we need dis?
+
+plot(small$loggingSample, y <- small$locationLongitude)
+# longitude increased very fast at first, dropped a bit, increased again, then dropped fast
+
+IDnum <- small$loggingSample
+
+plot(IDnum, small$locationAltitude)
+# it decreased, kinda in a linear manner, gun try a reg on it
+reg_altitude <- lm(small$locationAltitude~IDnum)
+abline(reg_altitude, col = "red") 
+summary(reg_altitude)
+# R^2 is only 0.6, not so good, prolly its not linear
+
+plot(IDnum, small$locationSpeed)
+#a total mess, cant see any pattern, maybe we should pay more attention on this 
+
+plot(IDnum, small$locationCourse)
+# same as above, but what does location course even mean?
+
+plot(IDnum, small$locationVerticalAccuracy)
+#interesting discrete step funtion, why they are behaving like dis?
+
+plot(IDnum, small$locationHorizontalAccuracy)
+# something like the vertical accuracy, do we need these two in the analysis?
+
+
+
+
+plot(IDnum, small$accelerometerTimestamp_sinceReboot)
+#a straight line
+plot(IDnum, small$motionUserAccelerationX)
+# some pretty messed up distribution of seperate dots, prolly need to take a look on this !!!!!!!!!!!!!!!!
+plot(IDnum, small$motionUserAccelerationY)
+# same as above
+plot(IDnum, small$motionUserAccelerationZ)
+# the same
+
+
+
+plot(IDnum, small$gyroTimestamp_sinceReboot)
+# a straight line 
+plot(IDnum, small$gyroRotationX)
+plot(IDnum, small$gyroRotationY)
+plot(IDnum, small$gyroRotationZ)
+# basically the same as the acceleration data
+
+
+plot(IDnum, small$motionTimestamp_sinceReboot)
+# a line
+plot(IDnum, small$motionYaw)
+# a weird W shape distribution, what is this?????
+
+plot(IDnum, small$motionRoll)
+# looks like an M, dense on the top
+
+plot(IDnum, small$motionPitch)
+# like an upside down version of the previous one, dense on the bottom
+
+plot(IDnum, small$motionRotationRateX)
+plot(IDnum, small$motionRotationRateY)
+plot(IDnum, small$motionRotationRateZ)
+# dense around x = 0, the three look pretty similar
+
+plot(IDnum, small$motionUserAccelerationX)
+plot(IDnum, small$motionUserAccelerationY)
+plot(IDnum, small$motionUserAccelerationZ)
+# same behavior, like the rotation rates
+
+class(small$motionAttitudeReferenceFrame)
+length(unique(small$motionAttitudeReferenceFrame))
+# the output is 1, so there is only one character in this column
+
+plot(IDnum, small$motionQuaternionX)
+plot(IDnum, small$motionQuaternionY)
+plot(IDnum, small$motionQuaternionZ)
+plot(IDnum, small$motionQuaternionW)
+# the X one is all over the place, but bottom dense,
+# increased very fast, but dropped very fast in the end
+#a W shaped thing
+# same as Z
+
+plot(IDnum, small$motionGravityX)
+plot(IDnum, small$motionGravityY)
+plot(IDnum, small$motionGravityZ)
+# X and Y behave alike, but Z has an increasing trend
+
+plot(IDnum, small$motionMagneticFieldX)
+plot(IDnum, small$motionMagneticFieldY)
+plot(IDnum, small$motionMagneticFieldZ)
+# didnt think info on magnet field has anything to do with the analysis, but plotted them out anyway
+length(unique(small$motionMagneticFieldCalibrationAccuracy))
+# = 1
+
+plot(IDnum, small$activityTimestamp_sinceReboot)
+# an increasing step function
+
+length(unique(small$activityActivityConfidence))
+# = 2
+unique(small$activityActivityConfidence)
+# = 2 and 1
+
+summary(small$activityActivityStartDate)
+unique(small$activityActivityStartDate)
+# I dont know why there are 5 start dates in a single file??????? Shouldn't be only one tho?
+
+# didn't do the pedometer data, since I don't think thats useful
+
+plot(small$altimeterReset)
+# all 0
+
+plot(IDnum, small$altimeterRelativeAltitude)
+# a N shape graph, looks familiar
+
+plot(small$altimeterPressure)
+# inverse of the previous one, weird
+
+# IP addresses are ommitted 
+
+unique(small$deviceOrientation)
+# = 1
+
+# the rest are ommited as well, since little useful info contained
