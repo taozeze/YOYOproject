4e25d60171042013fa2aca5e457d8443d851b02a
xkuang <xk2120@columbia.edu>
2016-12-18 11:10:37 -0500

    Sunday Morning workout

--- sensor.R
+++ sensor.R
@@ -8,8 +8,8 @@ library(Ecdat)
 library(fpp)
 library(forecast)
 
-N1 <- read.csv("2016-12-15_12-00-32.csv", header = TRUE, stringsAsFactors = FALSE)
-E1 <- read.csv("2016-12-15_12-00-36.csv", header = TRUE, stringsAsFactors = FALSE)
+E1 <- read.csv("2016-12-15_12-00-32.csv", header = TRUE, stringsAsFactors = FALSE)
+N1 <- read.csv("2016-12-15_12-00-36.csv", header = TRUE, stringsAsFactors = FALSE)
 
 toSeconds <- function(x){
   if (!is.character(x)) stop("x must be a character string of the form H:M:S")
@@ -40,6 +40,10 @@ N2 <- dplyr::select(N1, Time,locationAltitude,locationSpeed,accelerometerAcceler
                       motionQuaternionY,motionQuaternionZ,motionQuaternionW,motionGravityX,motionGravityY,motionGravityZ,+
                       motionMagneticFieldX,motionMagneticFieldY,motionMagneticFieldZ)
 
+N3 <- tidyr::gather(N2,Time)
+names(N3) <- c("time","variable","value")
+ggplot(N3)+ geom_line(aes(x=time, y=value))+facet_wrap(~variable, scales = "free")
+
 E2 <- dplyr::select(E1, Time,locationAltitude,locationSpeed,accelerometerAccelerationX,accelerometerAccelerationY,+
                       accelerometerAccelerationZ,gyroRotationX,gyroRotationY,gyroRotationZ,motionYaw,+
                       motionRoll,motionPitch,motionRotationRateX,motionRotationRateY,motionRotationRateZ,motionUserAccelerationX,+
@@ -47,12 +51,25 @@ E2 <- dplyr::select(E1, Time,locationAltitude,locationSpeed,accelerometerAcceler
                       motionQuaternionW,motionGravityX,motionGravityY,motionGravityZ,motionMagneticFieldX,motionMagneticFieldY,+
                       motionMagneticFieldZ)
 
-as.numeric(D$Time)
+E3 <- tidyr::gather(E2,Time)
+names(E3) <- c("time","variable","value")
+ggplot(E3)+ geom_line(aes(x=time, y=value))+facet_wrap(~variable, scales = "free")
+
+E3$name <- "expert"
+N3$name <- "naive"
+names(E3) <- c("time","variable","value","name")
+names(N3) <- c("time","variable","value","name")
+D1 <- dplyr::bind_rows(N3, E3)
+
+ggplot(D1)+ geom_line(aes(x=time, y=value,color=name))+facet_wrap(~variable, scales = "free")
+
+
 
 E2$name <- "expert"
 N2$name <- "naive"
 D <- dplyr::bind_rows(N2, E2)
 D$name<-ifelse(D$name=="naive",0,1)
+D <- as.data.frame(D)
 
 COR <- cor(D)
 corrplot(COR, order="AOE", method="circle", tl.pos="lt", type="upper",        
@@ -60,15 +77,9 @@ corrplot(COR, order="AOE", method="circle", tl.pos="lt", type="upper",
          addCoef.col="black", addCoefasPercent = TRUE,
          sig.level=0.01, insig = "blank")
 
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(locationAltitude))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(locationAltitude))
-locationAltitude <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(locationAltitude) <- c("time","naive","expert")
-ggplot(locationAltitude) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("locationAltitude")
+pairs(D)
 
-
+#locationSpeed is +0.41 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(locationSpeed))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(locationSpeed))
 locationSpeed <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -77,54 +88,7 @@ ggplot(locationSpeed) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("locationSpeed")
 
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationX))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationX))
-accelerometerAccelerationX <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(accelerometerAccelerationX) <- c("time","naive","expert")
-ggplot(accelerometerAccelerationX) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("accelerometerAccelerationX")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationY))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationY))
-accelerometerAccelerationY <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(accelerometerAccelerationY) <- c("time","naive","expert")
-ggplot(accelerometerAccelerationY) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("accelerometerAccelerationY")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationZ))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(accelerometerAccelerationZ))
-accelerometerAccelerationZ <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(accelerometerAccelerationZ) <- c("time","naive","expert")
-ggplot(accelerometerAccelerationZ) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("accelerometerAccelerationZ")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(gyroRotationX))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(gyroRotationX))
-gyroRotationX <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(gyroRotationX) <- c("time","naive","expert")
-ggplot(gyroRotationX) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("gyroRotationX")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(gyroRotationY))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(gyroRotationY))
-gyroRotationY <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(gyroRotationY) <- c("time","naive","expert")
-ggplot(gyroRotationY) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("gyroRotationY")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(gyroRotationZ))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(gyroRotationZ))
-gyroRotationZ <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(gyroRotationZ) <- c("time","naive","expert")
-ggplot(gyroRotationZ) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("gyroRotationZ")
-
+#locationSpeed is +0.7 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionYaw))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionYaw))
 motionYaw <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -133,6 +97,7 @@ ggplot(motionYaw) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionYaw")
 
+#motionRoll is -0.43 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionRoll))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionRoll))
 motionRoll <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -141,38 +106,7 @@ ggplot(motionRoll) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionRoll")
 
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionPitch))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionPitch))
-motionPitch <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(motionPitch) <- c("time","naive","expert")
-ggplot(motionPitch) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("motionPitch")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionRotationRateX))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionRotationRateX))
-motionRotationRateX <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(motionRotationRateX) <- c("time","naive","expert")
-ggplot(motionRotationRateX) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("motionRotationRateX")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionRotationRateY))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionRotationRateY))
-motionRotationRateY <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(motionRotationRateY) <- c("time","naive","expert")
-ggplot(motionRotationRateY) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("motionRotationRateY")
-
-E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionRotationRateZ))
-N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionRotationRateZ))
-motionRotationRateZ <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
-names(motionRotationRateZ) <- c("time","naive","expert")
-ggplot(motionRotationRateZ) + geom_line(aes(time, naive, color="naive")) + 
-  geom_line(aes(time, expert, color="expert")) + 
-  xlab("Time(s)") + ylab("motionRotationRateZ")
-
+#motionQuaternionX is -0.75 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionQuaternionX))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionQuaternionX))
 motionQuaternionX <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -181,6 +115,7 @@ ggplot(motionQuaternionX) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionQuaternionX")
 
+#motionQuaternionY is +0.67 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionQuaternionY))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionQuaternionY))
 motionQuaternionY <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -189,6 +124,7 @@ ggplot(motionQuaternionY) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionQuaternionY")
 
+#motionQuaternionZ is +0.98 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionQuaternionZ))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionQuaternionZ))
 motionQuaternionZ <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -197,6 +133,7 @@ ggplot(motionQuaternionZ) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionQuaternionZ")
 
+#motionQuaternionW is +0.96 correlated with expert level
 E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionQuaternionW))
 N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionQuaternionW))
 motionQuaternionW <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
@@ -205,19 +142,16 @@ ggplot(motionQuaternionW) + geom_line(aes(time, naive, color="naive")) +
   geom_line(aes(time, expert, color="expert")) + 
   xlab("Time(s)") + ylab("motionQuaternionW")
 
+#motionGravityZ is -0.45 correlated with expert level
+E3 <- E2 %>% group_by(Time) %>% summarise(mean(motionGravityZ))
+N3 <- N2 %>% group_by(Time) %>% summarise(mean(motionGravityZ))
+motionGravityZ <- as.data.frame(dplyr::inner_join(N3, E3, by="Time"))
+names(motionGravityZ) <- c("time","naive","expert")
+ggplot(motionGravityZ) + geom_line(aes(time, naive, color="naive")) + 
+  geom_line(aes(time, expert, color="expert")) + 
+  xlab("Time(s)") + ylab("motionGravityZ")
 
 
 
 
-#  motionUserAccelerationX,motionUserAccelerationY,motionUserAccelerationZ,+
-#  motionGravityX,motionGravityY,motionGravityZ,+
-#  motionMagneticFieldX,motionMagneticFieldY,motionMagneticFieldZ)
-
-
-
-#E3 <- dplyr::select(E2, loggingTime)
-#E3 <- unique(E3)
-#N3 <- dplyr::select(N2, loggingTime)
-#N3 <- unique(N3)
-#D3 <- dplyr::inner_join(N3, E3, by="loggingTime")
 
